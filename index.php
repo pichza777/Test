badeb7813e8d267b7d08697bbc05c1255ae9f2c5
pichza777 <pichza777@gmail.com>
2016-06-22 23:55:48 +0700

    d

---
+++ index.php
@@ -0,0 +1,250 @@
+﻿<!DOCTYPE html>
+<html lang="th">
+<head>
+<title>9'50 Records</title>
+<meta charset="utf-8">
+<meta name = "format-detection" content = "telephone=no" />
+<link rel="icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" href="css/contact-form.css">
+<link rel="stylesheet" href="css/touchTouch.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/style.css">
+<link href='https://fonts.googleapis.com/css?family=Kanit:400,600,400italic&subset=thai,latin' rel='stylesheet' type='text/css'>
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/script.js"></script> 
+<script src="js/superfish.js"></script>
+<script src="js/jquery.equalheights.js"></script>
+<script src="js/jquery.mobilemenu.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/touchTouch.jquery.js"></script>
+<script src="js/TMForm.js"></script>
+<script src="js/modal.js"></script>
+
+<script>
+ $(window).load(function(){
+  $().UItoTop({ easingType: 'easeOutQuart' });
+
+  $("#owl").owlCarousel({
+    items : 4, //10 items above 1000px browser width
+    itemsDesktop : [800,1], //5 items between 1000px and 901px
+    itemsDesktopSmall : [601, 1], // betweem 900px and 601px
+    itemsTablet: [0, 1], //2 items between 600 and 0
+    /*itemsMobile : [479, 1], // itemsMobile disabled - inherit from itemsTablet option*/
+    navigation : true,
+    pagination :  false
+  });
+    $('.gallery .gall_item').touchTouch();
+ }); 
+
+
+
+</script>
+<!--[if lt IE 8]>
+ <div style=' clear: both; text-align:center; position: relative;'>
+   <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+     <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+   </a>
+</div>
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<link rel="stylesheet" media="screen" href="css/ie.css">
+<![endif]-->
+</head>
+
+<body >
+<!--==============================
+              header
+=================================-->
+<div class="menu_bg"></div>
+<section id="top" class="page page_bg__2">
+<header>
+  <div class="navigation single-page-nav">
+    <div class="container_12">
+      <div class="grid_12">
+        <h1 class="logo">
+          <a href="#top"><img src="images/logo.png" alt="">
+          </a>
+        </h1>
+        <nav>
+          <ul>
+           
+           <li><a href="#job">Services</a></li>
+           <li><a href="#about">Portfolio</a></li>
+		   <li><a href="#me" class="current">About Us</a></li>
+           <li><a href="#contacts">Contact Us</a></li>
+         </ul>
+        </nav>
+      </div> 
+    </div>
+  </div>    
+  
+  <div class="container_12 page_bg__2" >
+    <div class="grid_12 ">     
+	<h2><center>
+          <img src="images/logo-950-2.png" width="30%" height="30%" alt="">
+        </center>
+      
+        <span></span>
+      </h2>      
+      <h4>Think about sound , Think 9'50<div class="socials">
+        <a href="https://www.facebook.com/950Records" target="_blank" class="fa fa-facebook"></a>
+      </div>
+       
+      </h4> 
+      
+   </div> 
+    <div class="clear"></div>  
+  </div> 
+</header>
+</section>
+<!--=====================
+          Content
+======================-->
+<section id="me" class="page page_bg__2">
+<BR><BR><BR>
+  <div class="container_12">
+    <div class="grid_12" >
+
+	  
+                    กลุ่ม 9'50 เป็นกลุ่มคนดนตรีที่มีประสบการณ์ในการสร้างสรรค์งานทางด้านเสียงในทุกรูปแบบ
+อาทิเช่น ผลิตงานเพลงเชิงพาณิชย์ ผลิตเพลงสำหรับองค์กร และศิลปินในทุกๆระดับ
+รับจ้างบันทึกเสียงทั้งเสียงสดทั้งในและนอกสถานที่ และ บริการบันทึกเสียงในรูปแบบสตูดิโอ 
+บันทึกและจัดการเสียงในกองถ่าย สามารถแต่ง และทำสกอร์สำหรับโฆษณา ภาพยนต์ หนังสั้น รายการต่างๆ 
+และสามารถถ่ายทำโฆษณาได้เช่นกัน ทั้งหมดนี้ถูกสร้างสรรค์ผ่านทุกขั้นตอนอย่างมีประสบการ์ณ
+
+              </div>
+      
+      <div class="clear"></div>
+  </div>
+</section>
+<section id="job" class="page page_bg__2">
+  <div class="container_12">
+    <div class="grid_12">
+      <h3 class="head__1">Services</h3>
+      <h4>
+<h6>- รับจ้างบันทึกเสียงระดับเดโม่ เพื่อส่งค่ายเพลง ประกอบอัลบั้ม หรือส่งตามงานประกวดต่างๆ </h6><br>
+ทางกลุ่มเรามีสถานที่รับบันทึกเสียง พร้อมทีมงานที่มีประสบการ์ณให้ความช่วยเหลือ และอำนวยความสะดวกตลอดการบันทึกเสียง 
+และคอยให้คำแนะนำ และชี้แนะ สำหรับศิลปินมือใหม่ หรือ ตอบสนองความต้องการของศิลปินที่มีประสบการ์ณได้เป็นอย่างดี <br><br>
+
+<h6>-รับจ้างบันทึกเสียงสดนอกสถานที่ </h6><br>
+ทางทีมงาน 9'50 สามารถให้บริการลูกค้าทุกท่านในการบันทึกเสียงต่างๆของลูกค้า
+ไม่ว่าจะเป็นการแสดงดนตรีสด คอนเสิร์ต รายการทีวี งานสัมนา ทุกอย่างที่ท่านต้องการให้เราบันทึก
+เราสามารถบันทึกทุกเสียงที่คุณต้องการได้ทั้งในและนอกสถานที่ ด้วยอุปกรณ์ที่เพียบพร้อม
+เราพร้อมบริการท่านด้วยบุคลากรที่มีประสบการณ์ในสายงานด้านนี้อย่างเชี่ยวชาญ <br><br>
+
+<h6>- รับจ้างบันทึกเสียงตามกองถ่าย</h6><br>
+ทางทีมงาน 9'50 สามารถให้บริการลูกค้าในการบันทึกเสียงในกองถ่าย (sound on location) 
+และสามารถจัดทำ mixing เสียงเข้ากับทางภาพเคลื่อนไหวได้<br><br>
+
+<h6>- รับจ้างผลิตเพลงเชิงพาณิชย์ </h6><br>
+ทางกลุ่มเราสามารถรับจ้างผลิตเพลงให้กับลูกค้าทุกท่านได้ทุกรูปแบบ 
+ไม่ว่าจะเป็นการผลิตเพลงประกอบ score หนังสั้น ซีรี่ยส์ โฆษณา สารคดี เกมโชว์ 
+สามารถแต่งเนื้อร้อง หรือทำนองสำหรับศิลปินที่ต้องการทำเพลงประกอบอัลบั้ม 
+เพลงประจำองค์กร เพลงประกอบการแข่งขัน 
+ประกอบเสียงรอสาย และเพื่อการพาณิชย์ทุกประเภท 
+และสามารถออกแบบธีมเพลงเฉพาะตัวละครเกมต่างๆได้ และสามารถทำ sound effect ประกอบได้เช่นกัน
+ <br><br>
+
+<h6>- รับ mix&Mastering </h6><br>
+ทางเรารับทำการ mix&Mastering งานทุกประเภท
+ไม่ว่าจะเป็นงานที่เกี่ยวกับทีวี วิทยุ เพลง หรืองานคอนเสิร์ต
+เพื่อเสียงที่คุณต้องการใช้งานนั้น มีคุณภาพที่ดีและเสถียรมากยิ่งขึ้น
+เราทำงานด้วยทีมงานที่มีประสบการณ์หลายปี   <br><br>
+
+<h6>- รับอัดและผลิต spot วิทยุโฆษณาทุกรูปแบบ </h6><br>
+เราสามารถผลิต spot วิทยุโฆษณา ตามความต้องการของรายการนั้นๆได้ 
+รวมถึงผลิตงานเพลงที่ใช้ประกอบสื่อหรือรายการทางทีวี วิทยุ อินเตอร์เนททุกรูปแบบ <br><br>
+<br><br>
+</h4>
+    </div>
+    <div class="clear"></div>
+  </div>
+</section>
+<section id="about" class="page page_bg__2">
+  
+    <div class="container_12">
+      <div class="grid_12">
+        <h3 class="head__1">Portfolio</h3>
+        </div>
+		  
+			<img src="images/yt-logo.png" width="3%" height="4%" alt="">  
+			<a href="https://www.youtube.com/channel/UCGHliPTX4GrDSpTdrz3HrAQ/feed" target="_blank">9'50 Records Channel</a>
+		<center>
+		<iframe width="350" height="200" src="https://www.youtube.com/embed/qjd1QnC3IY8" frameborder="0" allowfullscreen></iframe>
+		<iframe width="350" height="200" src="https://www.youtube.com/embed/DaPSi4j5tRw" frameborder="0" allowfullscreen></iframe>
+		<iframe width="350" height="200" src="https://www.youtube.com/embed/lylpoYeb8RY" frameborder="0" allowfullscreen></iframe>
+		</center>
+      </div>
+      <div class="clear"></div>
+	  <br>
+	  <br>
+	  <br>
+	    <div class="clear"></div>  
+		  <div class="clear"></div>  
+		    <div class="clear"></div>  
+    </div>
+  
+</section>
+<section id="contacts" class="page">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.2266858362605!2d100.65286331424079!3d13.82542229930939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d62f9e85878ad%3A0x2b03edb5d0bc62a3!2sSoi+Nawamin+145%2C+Khwaeng+Nuanchan%2C+Khet+Bueng+Kum%2C+Krung+Thep+Maha+Nakhon+10230%2C+Thailand!5e0!3m2!1sen!2s!4v1465305119046" style="border:0"></iframe>
+  <div class="contacts_bg">
+    <div class="container_12">
+      <div class="grid_12">
+        <h3 class="head__1"><font color="#000000">Contact Us </font></h3>
+		<h4><br>
+		ที่อยู่ : 42/38 ถนนนวมินทร์ 145 เขต บึงกุ่ม แขวง นวลจันทร์ กรุงเทพมหานคร 10230
+<br>
+เบอร์ติดต่อ : 081-3081741 (คุณทัศน์ทยา)
+<br>
+081-4584850 (คุณทยากร)
+<br>
+Email: 9.50Records@gmail.com
+</h4>
+      </div>
+     
+    </div>
+    <div class="clear"></div>
+	
+  </div>
+</section>
+<!--==============================
+              footer
+=================================-->
+<footer id="footer">
+  <div class="container_12">
+    <div class="grid_12"> 
+      <div class="copyright"><span class="color1">9'50 Records</span> &copy; <span id="copyright-year">2016</span> | 
+       <div class="sub-copy">Website designed by <a href="http://www.templatemonster.com/" rel="nofollow">TemplateMonster.com</a></div>
+	   <div class="sub-copy">Imprement by 9'50 Records</a></div>
+      </div>
+    </div>
+  </div>  
+  <div class="clear"></div>
+</footer>
+<a href="#" id="toTop" class="fa fa-chevron-up"></a>
+<script src="js/jquery.singlePageNav.min.js"></script>
+        <script>
+
+            // Prevent console.log from generating errors in IE for the purposes of the demo
+            if ( ! window.console ) console = { log: function(){} };
+
+            // The actual plugin
+            $('.single-page-nav').singlePageNav({
+                offset: $('.single-page-nav').outerHeight(),
+                filter: ':not(.external)',
+                updateHash: true,
+                beforeStart: function() {
+                    console.log('begin scrolling');
+                },
+                onComplete: function() {
+                    console.log('done scrolling');
+                }
+            });
+        </script>
+</body>
+</html>
